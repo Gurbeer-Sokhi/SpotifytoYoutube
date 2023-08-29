05a4045744d325ee080ed65629050a9042c2ad8e
Gurbeer-Sokhi <gsokhi@stevens.edu>
2023-08-29 01:27:06 -0400

    implemented refresh token feature

--- server-side/data/Spotify.js
+++ server-side/data/Spotify.js
@@ -1,7 +1,15 @@
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const axios = require("axios");
+const { response } = require("express");
+const FormData = require("formdata");
 const querystring = require("querystring");
+const request = require("request");
+const env = process.env;
+let refresh_token = env.refresh_token;
+// let access_token;
+const formData = new FormData();
+const fetch = require("node-fetch");
 
 const auth = async (redirect_uri, code) => {
   try {
@@ -22,15 +30,39 @@ const auth = async (redirect_uri, code) => {
       }
     );
 
-    if (spotifyResponse) {
-      console.log("Response", spotifyResponse.data.access_token);
-      return spotifyResponse.status;
+    if (spotifyResponse.status == 200) {
+      return spotifyResponse;
     } else {
-      return "fail";
+      return "invalid_token";
     }
   } catch (e) {
     console.log(e);
   }
 };
 
-module.exports = { auth };
+const RefreshToken = async () => {
+  console.log("refresh token ", env.refresh_token);
+  try {
+    let authOptions = {
+      method: "POST",
+      headers: {
+        Authorization:
+          "Basic " +
+          new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `grant_type=refresh_token&refresh_token=${env.refresh_token}`,
+    };
+
+    let new_token = await fetch(
+      "https://accounts.spotify.com/api/token",
+      authOptions
+    );
+    // console.log(new_token.data);
+    return new_token;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { auth, RefreshToken };
--- server-side/routes/Spotify.js
+++ server-side/routes/Spotify.js
@@ -8,10 +8,11 @@ const redirect_uri_auth = "http://localhost:5000/profile";
 const querystring = require("querystring");
 const axios = require("axios");
 const Spotify = index.Spotify;
+const env = process.env;
 
 const scope = "user-read-private user-read-email";
-const client_id = process.env.SPOTIFY_CLIENT_ID;
-const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const client_id = env.SPOTIFY_CLIENT_ID;
+const client_secret = env.SPOTIFY_CLIENT_SECRET;
 
 const generateRandomString = (length) => {
   let text = "";
@@ -42,17 +43,36 @@ router.route("/login").get(async (req, res) => {
 router.route("/account").get(async (req, res) => {
   try {
     let AuthResponse = await Spotify.auth(redirect_uri, req.query.code);
-    if (AuthResponse == 200) {
-      console.log("SUCCESS", AuthResponse);
+    if (AuthResponse.status == 200) {
+      env.refresh_token = AuthResponse.data.refresh_token;
+      res.redirect("/profile");
     } else {
-      console.log("fail");
+      res.redirect("/#" + querystring.stringify({ error: AuthResponse }));
     }
-    res.send("account");
   } catch (e) {
     console.log(e.message);
   }
 });
 
-router.route("/profile").get(async (req, res) => {});
+router.route("/profile").get(async (req, res) => {
+  res.send("profile");
+});
+
+router.route("/refresh_token").get(async (req, res) => {
+  try {
+    let refresh_token = req.query.refresh_token;
+    let access_token = await Spotify.RefreshToken(refresh_token);
+    console.log(
+      "returned access_token",
+      access_token.json().then((data) => {
+        console.log("data", data);
+        env.access_token = data.access_token;
+      })
+    );
+    res.send(access_token);
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 module.exports = router;
